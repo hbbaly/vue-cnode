69c43a7c721a171672ee6de02a5eed7d2ed8aafd
hbbaly <1422699902@qq.com>
2018-12-29 09:48:26 +0800

    搭建cnode手机端的vue框架，准备使用postcss,stylus,better-scroll,

---
+++ vue-cnode/src/components/scroll/loading.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="mf-loading-container">
+    <!-- <img src="./loading.gif"> -->
+    <img
+      src="./loading1.png"
+      alt="">
+    <span>正在刷新</span>
+  </div>
+</template>
+<script type="text/ecmascript-6">
+const COMPONENT_NAME = 'loading'
+export default {
+  'name': COMPONENT_NAME
+}
+</script>
+<style lang="less" scoped >
+.mf-loading-container {
+  img {
+    width: 40px;
+    height: 40px;
+    display: inline-block;
+    vertical-align: middle;
+    animation: mymove 1s infinite;
+    -webkit-animation: mymove 1s infinite;
+  }
+  span {
+    display: inline-block;
+    margin-left: 10px;
+    vertical-align: middle;
+    line-height: 40px;
+  }
+}
+
+@keyframes mymove {
+  from {
+    -webkit-transform: rotate(0deg);
+  }
+  to {
+    -webkit-transform: rotate(360deg);
+  }
+}
+</style>
